50631ef37fe4eb1d9a69a9ed324aecee076d08a0
CrispyBacon1999 <bacon.josh09@gmail.com>
2020-04-09 14:00:29 -0400

    Initial service worker setup

--- src/index.js
+++ src/index.js
@@ -8,4 +8,39 @@ import { firebaseConfig } from "./firebase";
 
 firebase.initializeApp(firebaseConfig);
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker
+      .register("service-worker.js")
+      .then(
+        function (registration) {
+          console.log("Registered service worker");
+        },
+        function (err) {
+          console.error("ServiceWorker registration failed: ", err);
+        }
+      )
+      .catch(function (err) {
+        console.log(err);
+      });
+  });
+} else {
+  console.warn("Service workers not supported in this browser.");
+}
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  console.log("beforeinstallprompt Event fired");
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  return false;
+});
+if (deferredPrompt) {
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice.then((choice) => {
+    console.log(choice);
+  });
+  deferredPrompt = null;
+}
+
 ReactDOM.render(<Navigation />, document.getElementById("root"));
